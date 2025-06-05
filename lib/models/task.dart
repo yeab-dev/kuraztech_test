77459f49e4846bd0560d400f7465108e0f1ee8a3
yeabsera <yeab.dev@gmail.com>
2025-06-05 15:29:33 +0300

    feat: add a task model

---
+++ lib/models/task.dart
@@ -0,0 +1,11 @@
+class Task {
+  final String taskId;
+  final String taskTitle;
+  final String taskDescription;
+
+  const Task({
+    required this.taskId,
+    required this.taskTitle,
+    required this.taskDescription,
+  });
+}
