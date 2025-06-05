2c2b23a4e4bf4b758fc719d195da7c55fa4c82f4
yeabsera <yeab.dev@gmail.com>
2025-06-05 16:29:37 +0300

    modify task card to support completed or not

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -2,10 +2,21 @@ class Task {
   final String taskId;
   final String taskTitle;
   final String taskDescription;
+  final bool completed;
 
   const Task({
     required this.taskId,
     required this.taskTitle,
     required this.taskDescription,
+    required this.completed,
   });
+
+  Task toggleCompleted() {
+    return (Task(
+      taskId: taskId,
+      taskTitle: taskTitle,
+      taskDescription: taskDescription,
+      completed: !completed,
+    ));
+  }
 }
